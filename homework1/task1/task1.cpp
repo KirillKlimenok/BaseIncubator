b6613df1348409bc5d5852da900fdd398c76a7ea
ivan.mamai00@mail.ru <250219As>
2021-03-04 22:35:25 +0300

    for edit

--- homework1/task1/task1.cpp
+++ homework1/task1/task1.cpp
@@ -4,5 +4,49 @@ using namespace std;
 
 int main()
 {
-    
+	int nMutants = 0, dayCounter = 0;
+	short procentInNight = 0;
+	int killInDay = 0;
+	bool isDayOrNight = true;
+
+	cout << "Please enter number of mutants: ";
+	cin >> nMutants;
+	cout << "\nPlease enter procent in night: ";
+	cin >> procentInNight;
+	cout << "\nPlease enter numbers kills in day: ";
+	cin >> killInDay;
+
+	if (nMutants == killInDay) {
+		cout << "Human win" << endl << "days for kills mutants: " << dayCounter;
+		return 0;
+	}
+
+	if (nMutants * procentInNight / 100 > killInDay) {
+		cout << "Mutants win";
+		return 0;
+	}
+
+	while (true) {
+		if (isDayOrNight) {
+			nMutants -= killInDay;
+			if (nMutants == 0 || nMutants < 0) {
+				cout << "Human win" << endl << "Days: " << dayCounter;
+				break;
+			}
+			isDayOrNight = false;
+		}
+
+		if ((nMutants * procentInNight / 100) > (killInDay) && (dayCounter == 1)) {
+			cout << "Mutants win";
+			return 0;
+		}
+		else {
+			nMutants += nMutants * procentInNight / 100;
+			isDayOrNight = true;
+			dayCounter++;
+		}
+
+	}
+
+	return 0;
 }
--- homework1/task3/task3.cpp
+++ homework1/task3/task3.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-   
+	int binaryNumber = 0;
+	int decimalNumber = 0;
+	int counter = 0;
 
+	cout << "Please, enter decimal number: ";
+	cin >> binaryNumber;
+
+	if (binaryNumber < 0) return -1;
+
+	do {
+		decimalNumber += binaryNumber % 10 * pow(2, counter);
+		binaryNumber /= 10;
+
+		counter++;
+
+		if (binaryNumber < 0.1) break;
+
+	} while (true);
+
+	cout << decimalNumber;
+
+	return 0;
 }
