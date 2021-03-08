5cdc6cf7fd1eb04a3fca3afc4482ad817865e9ad
ivan.mamai00@mail.ru <250219As>
2021-03-09 00:54:12 +0300

    done homework2

---
+++ homework2/task3/task3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double inputNumber = 0.0;
+
+	cout << "Please, enter number: ";
+	cin >> inputNumber;
+
+	if (!cin || inputNumber < 0) {
+		cout << "Error!";
+		return -1;
+	}
+	else {}
+
+	int number = inputNumber * 10;
+	cout << number % 10;
+
+}
---
+++ homework2/task4/task4.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int getRandomNumber(int min, int max)
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	// Равномерно распределяем рандомное число в нашем диапазоне
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+int main()
+{
+	int randomNumber = 0, inputNumber = 0;
+	bool isRepeat = true;
+
+
+	while (true) {
+		randomNumber = getRandomNumber(1, 100);
+
+		while (true) {
+			cout << "Please, enter your number" << endl;
+			cin >> inputNumber;
+
+			if (inputNumber > randomNumber) {
+				cout << "It's cold, we need a smaller number" << randomNumber << endl;
+			}
+			else if (inputNumber < randomNumber) {
+				cout << "It's cold, we need a higher number" << randomNumber << endl;
+			}
+			else {
+				cout << "Cool, you guessed it" << endl;
+				break;
+			}
+		}
+
+		cout << "Can we play some more ?" << endl;
+		cin >> isRepeat;
+
+		if (!isRepeat) {
+			break;
+		}
+		else {
+			cin.clear();
+			fflush(stdin);
+		}
+
+	}
+	return 0;
+}
