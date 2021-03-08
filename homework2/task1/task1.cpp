6d8125ea109e7fa9120eeed0273c3fe1e9174be2
ivan.mamai00@mail.ru <250219As>
2021-03-08 22:45:42 +0300

    task1

---
+++ homework2/task1/task1.cpp
@@ -0,0 +1,15 @@
+﻿#include "task1.h"
+
+int main()
+{
+	int firstNumber, secondNumber;
+
+	cout << "Please, enter 2 numbers: ";
+	cin >> firstNumber >> secondNumber;
+
+	TheSmallestCommonMultiple multiple;
+	cout << multiple.SCM(firstNumber, secondNumber);
+
+	return 0;
+}
+
