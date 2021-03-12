abb225b5cafeb74d4a207191d58e5520d8e9edd9
ivan.mamai00@mail.ru <250219As>
2021-03-12 22:11:12 +0300

    done task1

---
+++ homework3/task1/task1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double powerNumber(double number, double power);
+bool isCurrentPower(double number);
+
+int main()
+{
+	double number, power;
+
+	cout << "Please, enter number: ";
+	cin >> number;
+	cout << "and enter power: ";
+	cin >> power;
+
+	if (isCurrentPower(power)) {
+		cout << powerNumber(number, power);
+	}
+	else {
+		cout << "Incorrect input, please restart program" << endl;
+		return -1;
+	}
+	
+	return 0;
+}
+
+double powerNumber(double number, double power) {
+	for (int i = 1; i < power; i++)	number *= number;
+
+	return number;
+}
+
+bool isCurrentPower(double power) {
+	return (double)((int)power) == power;
+}
