303665989d2db85d76b1155faf339f1fb4db1b66
ivan.mamai00@mail.ru <250219As>
2021-03-12 23:12:19 +0300

    done tadk2

---
+++ homework3/task2/task2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+//7 15 31 63 127
+int main()
+{
+	int inputNumber = 0;
+	int x = 7;
+	int a = 4;
+
+	cin >> inputNumber;
+
+	for (int i = 0; i <= inputNumber; i++) {
+		if (i == x) {
+			cout << x << '	';
+			x = x + a * 2;
+			a *= 2;
+		}
+		else {
+
+		}
+	}
+
+	return 0;
+}
