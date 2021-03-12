d5ebb96a10229add5b0d80c0d66a74d42e9ee7b6
ivan.mamai00@mail.ru <250219As>
2021-03-12 23:22:54 +0300

    added: input check, number check

--- homework3/task2/task2.cpp
+++ homework3/task2/task2.cpp
@@ -5,19 +5,31 @@ using namespace std;
 int main()
 {
 	int inputNumber = 0;
-	int x = 7;
-	int a = 4;
+	int NumberWithUnits = 7;
+	int stepOfBinaryNumbers = 4;
 
+	cout << "Please, enter number: ";
 	cin >> inputNumber;
 
-	for (int i = 0; i <= inputNumber; i++) {
-		if (i == x) {
-			cout << x << '	';
-			x = x + a * 2;
-			a *= 2;
-		}
-		else {
+	if (!cin) {
+		cout << "Input error, please restart programm and try again";
+		return -1;
+	}
 
+	if (inputNumber < 7) {
+		cout << "no numbers";
+	}
+	else {
+
+		for (int i = 0; i <= inputNumber; i++) {
+			if (i == NumberWithUnits) {
+				cout << NumberWithUnits << '	';
+				NumberWithUnits = NumberWithUnits + stepOfBinaryNumbers * 2;
+				stepOfBinaryNumbers *= 2;
+			}
+			else {
+
+			}
 		}
 	}
 
