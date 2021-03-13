d018326492f199acc70c039f1760b8656be1257a
ivan.mamai00@mail.ru <250219As>
2021-03-13 14:40:47 +0300

    add task3 file

---
+++ homework3/task3/task3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nFibonachi = 0, counter = 0;
+	int inputNumber = 0;
+	int mass[2];
+
+	mass[0] = 0;
+	mass[1] = 1;
+
+	cout << "Please, enter number element: ";
+	cin >> inputNumber;
+
+	while (true) {
+		if (counter == inputNumber - 1) {
+			cout << nFibonachi;
+			break;
+		}
+
+		counter++;
+
+		mass[0] = mass[1];
+		mass[1] = nFibonachi;
+
+		nFibonachi = mass[0] + mass[1];
+	}
+
+	return 0;
+}
