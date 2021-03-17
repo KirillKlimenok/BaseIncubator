0ac12017a6b48483954ab460150bbbb37fde35f0
ivan.mamai00@mail.ru <250219As>
2021-03-18 00:55:26 +0300

    done task 2

---
+++ homework4/task2/task2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string enterInputData(string number, string str);
+string  printFlipNumber(string number);
+
+int main()
+{
+	string number = "";
+
+	number = enterInputData(number, "number: ");
+
+	cout << printFlipNumber(number);
+
+	return 0;
+}
+
+string enterInputData(string number, string str) {
+	cout << "Please, enter " << str;
+	cin >> number;
+	cout << endl;
+
+	return number;
+}
+
+string  printFlipNumber(string number) {
+	string flipNumber = "";
+
+	for (int i = number.length(); i > -1; i--) {
+		flipNumber += number[i];
+	}
+
+	return flipNumber;
+}
