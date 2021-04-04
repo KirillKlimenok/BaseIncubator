e091cd553c123455d1f1df533753742a15785de1
ivan.mamai00@mail.ru <250219As>
2021-04-05 01:22:27 +0300

    first prototype

--- homework6/task1/task1.cpp
+++ homework6/task1/task1.cpp
@@ -1,20 +1,65 @@
-﻿// task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+int massSize = 0;
+double sqare = 0;
+
+/*1. ПРОСВЕТ. Куб состоит из n3 прозрачных и непрозрачных кубиков. Имеется ли хотя бы один просвет?*/
+int getRandomNumber(int min, int max);
+bool isThereAnyClearance(int mass[]);
+int* fillingInTheArray();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int lineCube = 3;
+	massSize = pow(lineCube, 3);
+	sqare = pow(lineCube, 2);
+	int* cube = fillingInTheArray();
+	int smalCube = 1;
+
+	if (isThereAnyClearance(cube)) {
+		cout << "Yes";
+	}
+	else {
+		cout << "error";
+	}
+
+
+	return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int getRandomNumber(int min, int max)
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int* fillingInTheArray() {
+	int* cube = new int[massSize];
+
+	for (int i = 0; i < massSize; i++) {
+		cube[i] = getRandomNumber(0, 1);
+	}
+	
+	return cube;
+}
+
+bool isThereAnyClearance(int mass[]) {
+	bool isAnyClearance = false;
+
+	for (int i = 0; i < sqare; i++) {
+		for (int j = i; j < massSize; j += sqare) {
+			if (mass[j] == 1) {
+				isAnyClearance = true;
+			}
+			else {
+				isAnyClearance = false;
+				break;
+			}
+		}
+		if (isAnyClearance == true) break;
+	}
+
+	return isAnyClearance;
+}
